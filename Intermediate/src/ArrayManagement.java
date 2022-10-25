0403ab27b341bc280e04c3d7722cd88403e54024
DesiAPG <desireapgames@gmail.com>
2022-10-25 10:29:09 -0500

    Arrays

---
+++ Intermediate/src/ArrayManagement.java
@@ -0,0 +1,15 @@
+public class ArrayManagement {
+    public static void main(String[] args) {
+
+        String array[] = new String[4];
+
+        array[0] = "Duvan";
+        array[1] = "Camilo";
+        array[2] = "John";
+        array[3] = "David";
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+}
